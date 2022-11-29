96c16f089ec67abe45065fc51faac5f5843d5cfc
pedroalves616 <pedrojunior20132013@gmail.com>
2022-11-29 00:07:54 -0300

    TRISTELOL

---
+++ ATV3/Armstrong.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATV3
+{
+    internal class Armstrong
+    {
+        internal static bool IsArmstrong(int num)
+        {
+            int  temp =0 , sum = 0, rem;
+
+
+            while (num > 0)
+            {
+                rem = num % 10;
+                sum = sum + rem * rem * rem;
+                num = num / 10;
+            }
+
+            return temp == sum ? true : false;
+
+
+        }
+    }
+}
---
+++ ATV3/ArmstrongForms.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATV3
+{
+    internal class ArmstrongForms
+    {
+        public string Num { get; private set; }
+    }
+}
